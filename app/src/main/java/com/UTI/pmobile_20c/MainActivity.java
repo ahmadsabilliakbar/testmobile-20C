254bee457fe807191e918bebd86c0eec5b6d3fa4
aozen <aozenns@gmail.com>
2023-05-05 20:07:07 +0700

    uploud project pertama

---
+++ app/src/main/java/com/UTI/pmobile_20c/MainActivity.java
@@ -0,0 +1,14 @@
+package com.UTI.pmobile_20c;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
